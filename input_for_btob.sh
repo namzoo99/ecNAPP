c76fd52df60d3b3a81930c140ca93e3c919da98c
Yoonjoo <86759935+namzoo99@users.noreply.github.com>
2022-11-01 20:28:33 +0900

    Update input_for_btob.sh

--- input_for_btob.sh
+++ input_for_btob.sh
@@ -11,6 +11,6 @@ for i in $(seq 1 $END); do
 if [ -e ${path}/${barcode}_amplicon${i}.png ]; then
     echo "${barcode}    ${path}/${barcode}_amplicon${i}_cycles.txt    ${path}/${barcode}_amplicon${i}_graph.txt" >> ${out}/${barcode}-bp.input.txt
 else
-	exit 2
+        exit 2
 fi
 done
